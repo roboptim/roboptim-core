c537fee798716535c455b53ad9183476be4398a0
Thomas Moulard <thomas.moulard@gmail.com>
2009-03-30 18:59:11 +0200

    Add rough implementation of a CFSQP bridge.
    	* TODO: Add items.
    	* src/Makefile.am: Link against CFSQP.
    	* src/cfsqp.cc: Implement the bridge.
    
    Signed-off-by: Thomas Moulard <thomas.moulard@gmail.com>

--- src/cfsqp.cc
+++ src/cfsqp.cc
@@ -21,11 +21,90 @@
  *
  * \brief Implementation of the CFSQP module.
  */
+#include <limits>
+#include <cfsqpusr.h>
 
 #include "cfsqp.hh"
 
 namespace optimization
 {
+  typedef void (*fct_t) (int, int, double*, double*, void*);
+  typedef void (*grad_t) (int, int, double*, double*, fct_t, void*);
+
+  namespace detail
+  {
+    //FIXME: BEGIN should not duplicate that.
+//     static void
+//     vector_to_array (Solver::value_type* dst, const Solver::array_t& src)
+//     {
+//       memcpy (dst, &src[0], src.size () * sizeof (Solver::value_type));
+
+//       for (std::size_t i = 0; i < src.size (); ++i)
+//         assert (dst[i] = src[i]);
+//     }
+
+    static void
+    array_to_vector (Solver::array_t& dst, const Solver::value_type* src)
+    {
+      memcpy (&dst[0], src, dst.size () * sizeof (Solver::value_type));
+
+      for (std::size_t i = 0; i < dst.size (); ++i)
+        assert (dst[i] = src[i]);
+    }
+    //FIXME: END should not duplicate that.
+
+
+
+    void obj (int nparam, int j , double* x, double* fj, void* cd)
+    {
+      assert (cd);
+      CFSQPSolver* solver = static_cast<CFSQPSolver*> (cd);
+
+      CFSQPSolver::array_t x_ (nparam);
+      array_to_vector (x_, x);
+      *fj = solver->getFunction () (x_);
+    }
+
+    void constr (int nparam, int j,
+                 double* x, double* gj, void* cd)
+    {
+      assert (cd);
+      CFSQPSolver* solver = static_cast<CFSQPSolver*> (cd);
+
+      assert (solver->getConstraints ().size () - j == 0);
+
+      CFSQPSolver::array_t x_ (nparam);
+      array_to_vector (x_, x);
+
+      *gj = solver->getConstraints () [j].function (x_);
+    }
+
+    void gradob (int nparam, int j,
+                 double* x, double* gradf, fct_t dummy, void* cd)
+    {
+      assert (cd);
+      CFSQPSolver* solver = static_cast<CFSQPSolver*> (cd);
+
+      if (solver->getGradient ().empty ())
+        ::grobfd (nparam, j, x, gradf, dummy, cd);
+
+      //FIXME: should use gradient here.
+      ::grobfd (nparam, j, x, gradf, dummy, cd);
+    }
+
+    void gradcn (int nparam, int j,
+                 double* x, double* gradgj, fct_t dummy, void* cd)
+    {
+      assert (cd);
+      //CFSQPSolver* solver = static_cast<CFSQPSolver*> (cd);
+
+      //FIXME: if gradient of constraints available, use it else...
+      //if (!solver.getGradient ())
+      ::grcnfd (nparam, j, x, gradgj, dummy, cd);
+    }
+
+  }
+
   CFSQPSolver::CFSQPSolver (function_t fct,
                             size_type n,
                             gradient_t g,
@@ -42,6 +121,57 @@ namespace optimization
   CFSQPSolver::result_t
   CFSQPSolver::getMinimum () throw ()
   {
+    if (result_.which () != SOLVER_NO_SOLUTION)
+      return result_;
+
+    int nparam = getArity ();
+    int nf = 1; //FIXME: only one objective function.
+    int nfsr = 0;
+    int nineqn = 0;
+    int nineq = 0;
+    int neqn = 0;
+    int neq = 0;
+    int ncsrl = 0;
+    int ncsrn = 0;
+    int mesh_pts;
+    int mode = 100;
+    int iprint = 2;
+    int miter = 500;
+    int inform = 0;
+    double bignd = std::numeric_limits<value_type>::infinity ();
+    double eps = 1e-8;
+    double epseqn = 1e-8;
+    double udelta = 1e-8;
+    double bl[getArity ()];
+    double bu[getArity ()];
+    double x[getArity ()];
+    double f = 0.;
+    double g = 0.;
+    double lambda[getArity ()];
+    fct_t obj = detail::obj;
+    fct_t constr = detail::constr;
+    grad_t gradob = detail::gradob;
+    grad_t gradcn = detail::gradcn;
+    void* cd = this;
+
+    // Initialize bounds.
+    for (size_type i = 0; i < getArity (); ++i)
+      {
+        bl[i] = -std::numeric_limits<value_type>::infinity ();
+        bu[i] = std::numeric_limits<value_type>::infinity ();
+        lambda[i] = 1.; //FIXME: what's that?
+      }
+
+    // Copy starting point.
+    if (start_)
+      memcpy (x, &(*start_)[0], getArity () * sizeof (value_type));
+
+
+    cfsqp (nparam, nf, nfsr, nineqn, nineq, neqn, neq, ncsrl,  ncsrn,
+           &mesh_pts, mode,  iprint, miter, &inform, bignd, eps, epseqn,
+           udelta, bl, bu, x, &f, &g, lambda,
+           obj, constr, gradob, gradcn, cd);
+
     return result_;
   }
 
