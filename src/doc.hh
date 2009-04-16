b7719a166be290a6b9648f434a8a317c29c396d2
Thomas Moulard <thomas.moulard@gmail.com>
2009-04-16 15:52:10 +0200

    Document libraries design and how to add a new solver.
    	* src/doc.hh: Here.
    
    Signed-off-by: Thomas Moulard <thomas.moulard@gmail.com>

--- src/doc.hh
+++ src/doc.hh
@@ -51,6 +51,18 @@
    - \subpage faq
    - \subpage design
    - \subpage bridge
+
+
+   \section reporting Reporting bugs
+
+   As this package is still in its early development steps, bugs report
+   and enhancement proposals are highly welcomed.
+
+   To report a bug, please go this package's
+   <a href="http://www.assembla.com/wiki/show/liboptimization/">web page</a>.
+
+   To discuss about this package, please use
+   <a href="https://sympa.laas.fr/sympa/info/humanoid-optim">humanoid-optim@laas.fr</a>.
 */
 
 
@@ -111,26 +123,24 @@
    In the following sample, a TwiceDerivableFunction will be defined.
 
    \code
-class F : public TwiceDerivableFunction
+struct F : public TwiceDerivableFunction
 {
-  F () : Function (4)
+  F () : TwiceDerivableFunction (4, 1)
   {
-    // Set bound for all variables.
-    // 1. < x_i < 5. (x_i in [1.;5.])
-    for (int i = 0; i - n > 0; ++i)
-      argBounds[i] = std::make_pair(1., 5.);
   }
 
-  virtual value_type
+  virtual vector_t
   operator () (const vector_t& x) const throw ()
   {
-    return x[0] * x[3] * (x[0] + x[1] + x[2]) + x[3];
+    vector_t res (m);
+    res (0) = x[0] * x[3] * (x[0] + x[1] + x[2]) + x[3];
+    return res;
   }
 
   virtual gradient_t
-  gradient (const vector_t& x) const throw ()
+  gradient (const vector_t& x, int) const throw ()
   {
-    vector_t grad (n);
+    gradient_t grad (n);
 
     grad[0] = x[0] * x[3] + x[3] * (x[0] + x[1] + x[2]);
     grad[1] = x[0] * x[3];
@@ -140,7 +150,7 @@ class F : public TwiceDerivableFunction
   }
 
   virtual hessian_t
-  hessian (const vector_t& x) const throw ()
+  hessian (const vector_t& x, int) const throw ()
   {
     matrix_t h (n, n);
     h (0, 0) = 2 * x[3];
@@ -177,22 +187,25 @@ class F : public TwiceDerivableFunction
 
 
    \code
-class G0 : public TwiceDerivableFunction
+struct G0 : public TwiceDerivableFunction
 {
   G0 ()
-    : Function (4)
-  {}
+    : TwiceDerivableFunction (4, 1)
+  {
+  }
 
-  virtual value_type
+  virtual vector_t
   operator () (const vector_t& x) const throw ()
   {
-    return x[0] * x[1] * x[2] * x[3];
+    vector_t res (m);
+    res (0) = x[0] * x[1] * x[2] * x[3];
+    return res;
   }
 
   virtual gradient_t
-  gradient (const vector_t& x) const throw ()
+  gradient (const vector_t& x, int) const throw ()
   {
-    vector_t grad (n);
+    gradient_t grad (n);
 
     grad[0] = x[1] * x[2] * x[3];
     grad[1] = x[0] * x[2] * x[3];
@@ -200,30 +213,85 @@ class G0 : public TwiceDerivableFunction
     grad[3] = x[0] * x[1] * x[2];
     return grad;
   }
+
+  virtual hessian_t
+  hessian (const vector_t& x, int) const throw ()
+  {
+    matrix_t h (n, n);
+    h (0, 0) = 0.;
+    h (0, 1) = x[2] * x[3];
+    h (0, 2) = x[1] * x[3];
+    h (0, 3) = x[1] * x[2];
+
+    h (1, 0) = x[2] * x[3];
+    h (1, 1) = 0.;
+    h (1, 2) = x[0] * x[3];
+    h (1, 3) = x[0] * x[2];
+
+    h (2, 0) = x[1] * x[3];
+    h (2, 1) = x[0] * x[3];
+    h (2, 2) = 0.;
+    h (2, 3) = x[0] * x[1];
+
+    h (3, 0) = x[1] * x[2];
+    h (3, 1) = x[0] * x[2];
+    h (3, 2) = x[0] * x[1];
+    h (3, 3) = 0.;
+    return h;
+  }
 };
 
-class G1 : public TwiceDerivableFunction
+struct G1 : public TwiceDerivableFunction
 {
   G1 ()
-    : Function (4)
-  {}
+    : TwiceDerivableFunction (4, 1)
+  {
+  }
 
-  virtual value_type
+  virtual vector_t
   operator () (const vector_t& x) const throw ()
   {
-    return x[0]*x[0] + x[1]*x[1] + x[2]*x[2] + x[3]*x[3];
+    vector_t res (m);
+    res (0) = x[0]*x[0] + x[1]*x[1] + x[2]*x[2] + x[3]*x[3];
+    return res;
   }
 
   virtual gradient_t
-  gradient (const vector_t& x) const throw ()
+  gradient (const vector_t& x, int) const throw ()
   {
-    vector_t grad (n);
+    gradient_t grad (n);
 
     grad[0] = 2 * x[0];
     grad[1] = 2 * x[1];
     grad[2] = 2 * x[2];
     grad[3] = 2 * x[3];
     return grad;
+  }
+
+  virtual hessian_t
+  hessian (const vector_t& x, int) const throw ()
+  {
+    matrix_t h (n, n);
+    h (0, 0) = 2.;
+    h (0, 1) = 0.;
+    h (0, 2) = 0.;
+    h (0, 3) = 0.;
+
+    h (1, 0) = 0.;
+    h (1, 1) = 2.;
+    h (1, 2) = 0.;
+    h (1, 3) = 0.;
+
+    h (2, 0) = 0.;
+    h (2, 1) = 0.;
+    h (2, 2) = 2.;
+    h (2, 3) = 0.;
+
+    h (3, 0) = 0.;
+    h (3, 1) = 0.;
+    h (3, 2) = 0.;
+    h (3, 3) = 2.;
+    return h;
   }
 };
    \endcode
@@ -240,45 +308,63 @@ class G1 : public TwiceDerivableFunction
    - Solve the problem by calling minimum.
 
    \code
-int main ()
+int run_test ()
 {
   F f;
   G0 g0;
   G1 g1;
 
-  Problem<TwiceDerivableFunction, TwiceDerivableFunction> pb (f);
-  pb.constraints ().push_back (&g0);
-  pb.constraints ().push_back (&g1);
+  CFSQPSolver::problem_t pb (f);
 
-  // Set the starting point (optional).
-  Function::vector_t start (f.n);
+  // Set bound for all variables.
+  // 1. < x_i < 5. (x_i in [1.;5.])
+  for (Function::size_type i = 0; i < pb.function ().n; ++i)
+    pb.argBounds ()[i] = T::makeBound (1., 5.);
+
+  // Add constraints.
+  pb.addConstraint (&g0, T::makeUpperBound (25.));
+  pb.addConstraint (&g1, T::makeBound (40., 40.));
+
+  // Set the starting point.
+  Function::vector_t start (pb.function ().n);
   start[0] = 1., start[1] = 5., start[2] = 5., start[3] = 1.;
-  pb.startingPoint () = start;
+
+  initialize_problem (pb, g0, g1);
 
   // Initialize solver
-  IpoptSolver solver (pb);
+  CFSQPSolver solver (pb);
 
   // Compute the minimum and retrieve the result.
-  IpoptSolver::result_t res = solver.minimum ();
+  CFSQPSolver::result_t res = solver.minimum ();
+
+  // Display solver information.
+  std::cout << solver << std::endl;
 
   // Check if the minimization has succeed.
-  if (res.which () != IpoptSolver::SOLVER_VALUE)
+  if (res.which () != CFSQPSolver::SOLVER_VALUE_WARNINGS)
     {
       std::cout << "A solution should have been found. Failing..."
+                << std::endl
+                << boost::get<SolverError> (res).what ()
                 << std::endl;
       return 1;
     }
 
   // Get the result.
-  IpoptSolver::vector_t& result = boost::get<IpoptSolver::vector_t> (res);
+  Result& result = boost::get<ResultWithWarnings> (res);
 
   // Display the result.
   std::cout << "A solution has been found: " << std::endl;
   std::cout << result << std::endl;
-  std::cout << "f(*x) = " << solver.problem ().function () (result) << std::endl;
   return 0;
 }
    \endcode
+
+   This is the last piece of code needed to instantiate and resolve an optimization
+   problem with this package.
+
+   To see more usage examples, consider looking at the test directory of the
+   project which contains the project's test suite.
 */
 
 /**
@@ -354,21 +440,291 @@ int main ()
 /**
    \page design Design choices and libraries' internals
 
-   TO BE DONE
+   This package is divided in three parts:
+   <ul>
+   <li>Functions which represent different kind of mathematical functions and
+   theirs associated features (gradient, hessian...).</li>
+   <li>A problem class defines a whole optimization problem including some
+   technical details (scales).</li>
+   <li>Solvers hierarchy defines solvers that are working on a class of
+   optimizations problem (for instance QP solves problems where the
+   objective function is quadratic and the constraints are linear).</li>
+   </ul>
 
    \section fct The function hierarchy
+
+   The function hierarchy is an abstract hierarchy which defines
+   <emph>meta-function</emph> in the sense that the user has to derive from
+   these classes to define its <emph>real</emph> function.
+
+   \code
+   struct MyLinearFunction : public LinearFunction {
+   //FIXME: some code is missing
+   };
+
+   struct MyDerivableFunction : public DerivableFunction {
+   //FIXME: some code is missing
+   };
+   // etc...
+   \endcode
+
+   The user can defines as many functions as he wants as long as he respects
+   the constraints defined by the classes he inherits from:
+   <ul>
+   <li>Any function must be able to be evaluated (through operator()).</li>
+   <li>A derivable function has a gradient/jacobian.</li>
+   <li>A twice derivable function (\f$C^2\f$) has a hessian.</li>
+   <li>A quadratic function has the sames constraints than a \f$C^2\f$
+   function.</li>
+   <li>A linear function has the same constraints except its hessian which
+   is null and computed automatically by the abstract class.</li>
+   </ul>
+
+   It is important to note that all these functions are
+   \f$\mathbb{R}^n \rightarrow \mathbb{R}^m\f$ functions.
+
+   These functions have to be considered as m functions from to
+   \f$\mathbb{R}^n \rightarrow \mathbb{R}\f$.
+   In particular, it means that in the gradient/hessian methods, the
+   second argument (integer) refers to which function you want to get
+   the gradient/hessian.
+
+   It avoids useless computations when the whole Jacobian is not needed.
+   Hence, it also avoid the use of a tensor as this structure is particularly
+   costly and not built-in in uBlas (the matrix library the package rely on).
+
+
+   This set of meta-functions is completed by two generic implementations
+   of quadratic and linear functions in respectively NumericQuadraticFunction
+   and NumericLinearFunction.
+   Those are real functions as the user is meant to instantiate them, not
+   derive from them.
+
+
    \section pb The problem class
+
+   The problem class defines a complete optimization problem.
+   It is composed of:
+   <ul>
+   <li>A \f$\mathbb{R}^n \rightarrow \mathbb{R}\f$ cost
+   (or objective) function.</li>
+   <li>A set of functions constraints.</li>
+   <li>Bounds on the input arguments.</li>
+   <li>Bounds on the constraints functions output.</li>
+   <li>Scales on the input arguments.</li>
+   <li>Scales on the constraints functions output.</li>
+   </ul>
+
+   The problem class is templated by two parameters:
+   <ul>
+   <li>The objective's function type (F).</li>
+   <li>The set of constraint's types (C).</li>
+   </ul>
+
+   Those two parameters defines a class of problem.
+   For instance, QP solves problems where the objective function
+   is quadratic and the constraints are linear.
+
+   To a class of problem matches a set of solvers designed to
+   exactly those problems.
+
+   The copy constructor of the Problem class allows to make
+   the problem more generic by replacing F or C by a more
+   general type.
+
+   Examples:
+   <ul>
+   <li>It is valid to convert a linear problem into a non-linear
+   problem and make a QP solve it.</li>
+   <li>But, the opposite is not valid.</li>
+   </ul>
+
+   Ie: you can always go higher in the function hierarchy
+   but you can not go deeper.
+
+   However, this transformation as any dynamic typing makes
+   <emph>forget</emph> specific information and might slow-down the
+   process. Ie: solving a linear problem as a non-linear one
+   is slower than directly solving it.
+
+
    \section slv The solver hierarchy
+
+
+   The solver hierarchy is divided in three parts:
+   <ul>
+   <li>The generic solver.</li>
+   <li>The solver for a class of problems.</li>
+   <li>The bridges (the leafs in the hierarchy's tree).</li>
+   </ul>
+
+   \subsection gs Generic solver
+
+   The generic solver is an abstract class which is the root of the
+   hierarchy. All the solvers have to inherit from this class directly or
+   indirectly.
+
+   It only defines three methods:
+   <ul>
+   <li><code>solve</code> (virtual pure)</li>
+   <li><code>getMinimum</code></li>
+   <li><code>reset</code></li>
+   </ul>
+
+   The method <code>solve</code> has to fill the protected attribute
+   <code>result_</code>. It is implemented by the bridges at the bottom of the
+   hierarchy.
+
+   By default <code>result_</code> contains an instance the
+   <code>No_Solution</code> class.  It has to be changed to an instance of
+   <code>Result</code>, <code>ResultWithWarnings</code> or
+   <code>SolverError</code>.
+
+   <ul>
+   <li><code>No_Solution</code>: represents the fact that solve has never been
+   called. A user should never retrieve an instance of <code>No_Solution</code>.</li>
+   <li><code>Result</code>, <code>ResultWithWarnings</code>: represents a
+   <strong>valid</strong> result.  If <code>ResultWithWarnings</code> is used,
+   some non-critical problem might have happened and are specified in the object
+   (minor numerical instabilities for instance).</li>
+   <li><code>SolverError</code>: indicate that the optimization has
+   <strong>failed</strong>.</li>
+   </ul>
+
+   The <code>solve</code> method should rarely be called by the user.
+   Instead, the user calls <code>getMinimum</code> which calls
+   <code>solve</code> if required.
+   If <code>getMinimum</code> is called several time, the problem is only
+   solved once.
+
+   The <code>reset</code> method should be called when one wants to manually
+   force the recomputation of the solution.
+
+
+   \subsection solver Solver
+
+   The <code>Solver</code> class is parametrized by two parameters as the
+   <code>Problem</code> class.
+   It defines an abstract solver for a class of optimization problems.
+
+   For instance, a QP should inherit from
+   <code>Solver<QuadraticFunction, LinearFunction></code>.
+
+   This level of the hierarchy integrates the notion of <code>Problem</code>,
+   hence the <code>problem</code> method is defined to access the problem. Once
+   a solver is created with a problem, the problem can never be modified again.
+
+   \subsection briges Bridges
+
+   The leafs of the hierarchy are bridges.  The role of a bridge is to convert
+   the generic representation of the problem into the solver's representation.
+   It might also emulate some missing features of the back-end solver.
+
+   A bridge mainly implements the <code>solve</code> method. It may also
+   exposes the underlying solver's internal mechanism for fine tuning.
 */
 
 
 /**
    \page bridge How to link the library with a new solver?
 
-   TO BE DONE
 
-   \section slv How to extend the solver hierarchy?
+   This part assumes the reader went through the \ref design page and is
+   familiar with the user interface.
+
    \section tutorial Intregrating a solver step by step.
+
+   \subsection prereq Pre-requesite
+
+   The first step is determining if there is any interest in integrating
+   the solver.
+
+   The solver should target one of the following optimization problem's:
+   <ul>
+   <li>Non-linear optimization</li>
+   <li>Linear optimization</li>
+   </ul>
+   ...with or without constraints.
+
+   It should also support bounds and scales. If not, emulating them
+   should be possible.
+
+   Integrating solvers that solve very different problems will likely
+   not match the interface and lead to major changement in the package.
+   These changes are beyond the scope of this document and should be
+   discussed on the mailing-list.
+
+
+   \subsection pkg Modifying the package.
+
+   Basically, adding a new solver means adding a new leaf in the solver
+   hierarchy.
+
+   The new header and source files should be added in the <code>src</code>
+   directory.
+
+   The naming convention is CamlCase, the name should be the solver's name
+   suffixed by <code>Solver</code>.
+
+   It also should be pre-declared in the <code>fwd.hh</code> header.
+
+   A m4 macro should then be written to add a soft dependency against the
+   library (ie: the bridge is built if the solver is available, if not the
+   bridge is skipped).
+   Please consult the Autoconf manual and the <code>build-aux/cfsqp.m4</code>
+   and <code>build-aux/ipopt.m4</code> for more information.
+
+   \subsection impl Implementing the bridge.
+
+   The first step is choosing from which <code>Solver</code> you want to
+   inherit from. The <code>F</code> parameters should be straight-forward: it
+   is the kind of objective function you expect.
+   For the constraints, there is two cases:
+   <ul>
+   <li>Constraint's type are unique: set <code>C</code> to
+   <code>const MyConstraintType*</code> where <code>MyConstraintType</code>
+   is the solver's constraints type.</li>
+   <li>If you want to discriminate your constriants into different categories,
+   you will have to use a Boost.Variant. <code>C</code> should be:
+   <code>boost::variant<const MyConstraintType1*, const MyConstraintType2*>
+   </code>
+   </ul>
+
+   Then, the <code>solve</code> method should be implemented in the bridge.
+
+
+   Following guidelines should be respected by any bridge:
+   <ul>
+   <li>Be sure to call <code>reset</code> if the result has to be recomputed
+   the next time <code>getMinimum</code> is called.</li>
+   <li>Be sure that <code>solve</code> put the computed value in the
+   <code>result_</code> attribute.</li>
+   <li>Solver's arguments should be exposed to user through the class.</li>
+   <li>The solver should not display anything by default. If possible, use
+   the <code>print</code> method to display internal state.</li>
+   </ul>
+
+
+   \subsection ccl Conclusion
+
+   The dummy solver can be a good starting point for anyone who wants to add
+   a new solver as the code can be considered as the minimum stub to get a
+   compilable bridge. It should also be quite easy to understand.
+
+
+   To submit a new bridge, please post a patch on the project's mailing-list.
+   To generate a patch with Git:
+
+   \code
+   $ git diff HEAD > add_support_for_BRIDGENAME.patch
+   \endcode
+
+   (replace BRIDGENAME by the name of the solver you are integrating).
+   Then, post a message to the mailing-list. Please, prefix the object
+   by the following tag:
+   <pre>
+   [PATCH] Add support for BRIDGENAME.
+   </pre>
 */
 
 
