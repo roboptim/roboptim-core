4d19c50d367c934301c1e5b81aa9256b7a79ff3e
Thomas Moulard <thomas.moulard@gmail.com>
2009-04-28 20:30:04 +0900

    Fix various mistakes in deb package generation.
    	* THANKS: New.
    	* build-aux/deb.mk: Add clean rule, split deb rule.
    	* packages/debian/ChangeLog,
    	* packages/debian/changelog,
    	* packages/debian/control,
    	* packages/debian/rules: Fix various errors.
    
    Signed-off-by: Thomas Moulard <thomas.moulard@gmail.com>

--- build-aux/deb.mk
+++ build-aux/deb.mk
@@ -1,6 +1,6 @@
 include $(top_srcdir)/build-aux/init.mk
 
-DPKG_BUILDPACKAGE = dpkg-buildpackage -rfakeroot -us -uc -d
+DPKG_BUILDPACKAGE = dpkg-buildpackage -rfakeroot -E -W -d
 distdir = $(PACKAGE)-$(VERSION)
 DIST_ARCHIVE = $(distdir).tar.gz
 
@@ -12,11 +12,21 @@ EXTRA_DIST +=                                   \
 	docs					\
 	rules
 
-.PHONY: deb
+.PHONY: deb deb-prepare
 
-deb: $(top_builddir)/$(DIST_ARCHIVE)
+clean-local:
+	rm -rf $(PACKAGE)-$(VERSION)
+	rm -f $(DIST_ARCHIVE)
+	rm -f $(PACKAGE)_$(VERSION)-*.tar.gz
+	rm -f $(PACKAGE)_$(VERSION)-*.dsc
+	rm -f $(PACKAGE)_$(VERSION)-*.changes
+	rm -f $(PACKAGE)_$(VERSION)-*.deb
+
+deb-prepare:
 	cd $(top_builddir) && $(MAKE) $(AM_MAKEFLAGS) dist-gzip
 	rm -rf $(distdir)
-	tar xvzf $<
+	tar xzf $(top_builddir)/$(DIST_ARCHIVE)
 	ln -s packages/debian $(distdir)
+
+deb: deb-prepare
 	cd $(distdir) && $(DPKG_BUILDPACKAGE)
