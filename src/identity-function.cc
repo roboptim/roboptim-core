3244943fef1566a1d882d77eef4395023b6c53d1
Thomas Moulard <thomas.moulard@gmail.com>
2009-06-13 00:20:00 +0900

    Implement identity function.
    	* include/roboptim/core/identity-function.hh: New.
    	* src/Makefile.am: Add new source.
    	* src/identity-function.cc: New.
    	* tests/Makefile.am: Compile new test.
    	* tests/identity-function.cc: New.
    	* tests/identity-function.stdout: New.
    	* tests/testsuite.at: Run new test case.
    
    Signed-off-by: Thomas Moulard <thomas.moulard@gmail.com>

---
+++ src/identity-function.cc
@@ -0,0 +1,74 @@
+// Copyright (C) 2009 by Thomas Moulard, AIST, CNRS, INRIA.
+//
+// This file is part of the roboptim.
+//
+// roboptim is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// roboptim is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with roboptim.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <boost/numeric/ublas/io.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/vector.hpp>
+
+#include <roboptim/core/indent.hh>
+#include <roboptim/core/identity-function.hh>
+
+namespace roboptim
+{
+  IdentityFunction::IdentityFunction (const vector_t& offset)
+    throw ()
+    : LinearFunction (offset.size (), offset.size ()),
+      offset_ (offset)
+  {
+  }
+
+  IdentityFunction::~IdentityFunction () throw ()
+  {
+  }
+
+  void
+  IdentityFunction::impl_compute (result_t& result,
+				  const argument_t& argument)
+    const throw ()
+  {
+    using namespace boost::numeric::ublas;
+    result = argument + this->offset_;
+  }
+
+  void
+  IdentityFunction::impl_jacobian (jacobian_t& jacobian,
+				   const argument_t&) const throw ()
+  {
+    using namespace boost::numeric::ublas;
+    jacobian = identity_matrix<value_type> (jacobianSize ().first,
+					    jacobianSize ().second);
+  }
+
+  void
+  IdentityFunction::impl_gradient (gradient_t& gradient,
+					const argument_t& argument,
+					size_type idFunction) const throw ()
+  {
+    using namespace boost::numeric::ublas;
+    gradient.clear ();
+    gradient[idFunction] = 1.;
+  }
+
+  std::ostream&
+  IdentityFunction::print (std::ostream& o) const throw ()
+  {
+    return o << "Identity function" << incindent << iendl
+             << "offset = " << this->offset_ << iendl
+             << decindent;
+  }
+
+} // end of namespace roboptim
---
+++ tests/identity-function.cc
@@ -0,0 +1,51 @@
+// Copyright (C) 2009 by Thomas Moulard, AIST, CNRS, INRIA.
+//
+// This file is part of the roboptim.
+//
+// roboptim is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// roboptim is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with roboptim.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <iostream>
+#include <boost/numeric/ublas/io.hpp>
+
+#include "common.hh"
+#include <roboptim/core/identity-function.hh>
+
+using namespace roboptim;
+
+int run_test ()
+{
+  IdentityFunction::vector_t offset (4);
+  offset[0] = 12.;
+  offset[1] = 46.;
+  offset[2] = 2.;
+  offset[3] = -9.;
+
+  IdentityFunction id (offset);
+
+  IdentityFunction::vector_t x (4);
+  x.clear ();
+
+  std::cout
+    << id << std::endl
+    << "Evaluate: " << std::endl
+    << id (x) << std::endl
+    << "Gradient: " << std::endl
+    << id.gradient (x) << std::endl
+    << "Jacobian: " << std::endl
+    << id.jacobian (x) << std::endl;
+
+  return 0;
+}
+
+GENERATE_TEST ()
