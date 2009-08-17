be582ffb388970de2f7bcb7ec18ac413d9e289fa
Thomas Moulard <thomas.moulard@gmail.com>
2009-08-17 13:00:29 +0900

    Remove dots in macros to ensure Windows portability.
    	* src/debug.hh: Make CPP code more portable.
    
    Signed-off-by: Thomas Moulard <thomas.moulard@gmail.com>

--- src/debug.hh
+++ src/debug.hh
@@ -27,17 +27,17 @@
 #  define AllocTag2(p, desc)
 #  define AllocTag_dynamic_description(p, data)
 #  define AllocTag(p, data)
-#  define Debug(STATEMENT...)
+#  define Debug(STATEMENT)
 #  define Dout(cntrl, data)
 #  define DoutFatal(cntrl, data) LibcwDoutFatal(, , cntrl, data)
-#  define ForAllDebugChannels(STATEMENT...)
-#  define ForAllDebugObjects(STATEMENT...)
-#  define LibcwDebug(dc_namespace, STATEMENT...)
+#  define ForAllDebugChannels(STATEMENT)
+#  define ForAllDebugObjects(STATEMENT)
+#  define LibcwDebug(dc_namespace, STATEMENT)
 #  define LibcwDout(dc_namespace, d, cntrl, data)
 #  define LibcwDoutFatal(dc_namespace, d, cntrl, data) \
   do { ::std::cerr << data << ::std::endl; ::std::exit(EXIT_FAILURE); } while(1)
-#  define LibcwdForAllDebugChannels(dc_namespace, STATEMENT...)
-#  define LibcwdForAllDebugObjects(dc_namespace, STATEMENT...)
+#  define LibcwdForAllDebugChannels(dc_namespace, STATEMENT)
+#  define LibcwdForAllDebugObjects(dc_namespace, STATEMENT)
 #  define NEW(x) new x
 #  define CWDEBUG_ALLOC 0
 #  define CWDEBUG_MAGIC 0
