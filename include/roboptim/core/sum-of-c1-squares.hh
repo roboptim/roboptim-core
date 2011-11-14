25f30806630751dfb1eb5b104718b11d623a88cd
Florent Lamiraux <florent@laas.fr>
2011-11-14 16:16:01 +0100

    Add a class for scalar functions that are the sum of squares of scalar functions
        Class SumOfC1Squares represents the sum of differentiable scalar functions.
        This class enables to define non-linear least square problems.

---
+++ include/roboptim/core/sum-of-c1-squares.hh
@@ -0,0 +1,85 @@
+
+// Copyright (c) 2011 CNRS
+// Authors: Florent Lamiraux
+//
+// This file is part of the roboptim.
+//
+// roboptim is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// roboptim is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with roboptim.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef ROBOPTIM_CORE_SUM_OF_C1_SQUARES_HH
+# define ROBOPTIM_CORE_SUM_OF_C1_SQUARES_HH
+
+# include <boost/shared_ptr.hpp>
+
+# include <roboptim/core/derivable-function.hh>
+# include <roboptim/core/function.hh>
+
+namespace roboptim {
+  /// \brief Sum of the squares of differentiable functions
+  ///
+  /// This types enables roboptim to handle Levenberg Marquardt algorithm.
+  /// This algorithm indeed minimizes the sum of squares of differentiable
+  /// functions.
+  ///
+  /// The differentiable functions are stored in a vector valued function
+  /// called base function.
+  class SumOfC1Squares : public DerivableFunction
+  {
+  public:
+    /// @name Types
+    /// @{
+    /// \brief Parent type
+    typedef DerivableFunction parent_t;
+    typedef parent_t::argument_t argument_t;
+    typedef parent_t::jacobian_t jacobian_t;
+    typedef parent_t::gradient_t gradient_t;
+    typedef parent_t::jacobianSize_t jacobianSize_t;
+    typedef Function::size_type size_t;
+    typedef Function::value_type value_t;
+    /// @}
+    /// \brief Constructot by vector valued functions
+    /// The value of this scalar valued function is the sum of the
+    /// squares of the coordinates of the vector valued base function.
+    explicit SumOfC1Squares (const boost::shared_ptr<DerivableFunction>&
+			     function,
+			     const std::string& name) throw ();
+    explicit SumOfC1Squares (const SumOfC1Squares& function) throw ();
+    virtual ~SumOfC1Squares () throw ();
+    /// \brief Get base function
+    /// Base function is the vector valued function given at construction
+    /// of this class.
+    const boost::shared_ptr<const DerivableFunction>& baseFunction () const;
+  protected:
+    /// \brief Compute value of function
+    /// Value is sum of squares of coordinates of vector valued base function
+    virtual void
+    impl_compute(result_t &result, const argument_t &x) const throw ();
+    /// \brief Gradient
+    virtual void
+    impl_gradient(gradient_t& gradient, const argument_t& x,
+		  size_type row = 0) const throw ();
+  private:
+    /// Compute base function and store result in value_.
+    void computeFunction (const argument_t x) const;
+    /// \brief Vector valued function given at construction
+    boost::shared_ptr<const DerivableFunction> baseFunction_;
+    /// \brief Store last argument for which the function has been computed
+    mutable argument_t x_;
+    /// \brief temporary variable to store vector value of input function
+    mutable result_t value_;
+    /// \brief temporary variable to store gradients
+    mutable gradient_t gradient_;
+  }; // class Solver
+} // namespace roboptim
+#endif // ROBOPTIM_CORE_SUM_OF_C1_SQUARES_HH
---
+++ src/sum-of-c1-squares.cc
@@ -0,0 +1,97 @@
+
+// Copyright (c) 2011 CNRS
+// Authors: Florent Lamiraux
+//
+// This file is part of the roboptim.
+//
+// roboptim is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// roboptim is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with roboptim.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "roboptim/core/sum-of-c1-squares.hh"
+
+namespace roboptim {
+  bool operator!= (const Function::vector_t& x1,
+		   const Function::vector_t& x2)
+  {
+    if (x1.size() != x2.size()) return true;
+    for (Function::size_type i=0; i<x1.size(); i++) {
+      if (x1(i) != x2(i)) return true;
+    }
+    return false;
+  }
+
+  SumOfC1Squares::SumOfC1Squares (const boost::shared_ptr<DerivableFunction>&
+				  function,
+				  const std::string& name) throw () :
+    DerivableFunction(function->inputSize(), 1, name),
+    baseFunction_ (function)
+  {
+    value_.resize (function->outputSize());
+    gradient_.resize (function->inputSize());
+    x_.resize (function->inputSize());
+    x_.clear();
+    (*baseFunction_) (value_, x_);
+  }
+    
+  SumOfC1Squares::SumOfC1Squares (const SumOfC1Squares& src) throw ():
+    DerivableFunction(src.inputSize(), 1, src.getName()),
+    baseFunction_ (src.baseFunction_), x_ (src.x_),
+    value_ (src.value_),
+    gradient_ (src.gradient_)
+  {
+  }
+
+  SumOfC1Squares::~SumOfC1Squares () throw ()
+  {
+  }
+
+  const boost::shared_ptr<const DerivableFunction>& SumOfC1Squares::
+  baseFunction () const
+  {
+    return baseFunction_;
+  }
+
+  void SumOfC1Squares::
+  impl_compute(result_t &result, const argument_t &x) const throw ()
+  {
+    computeFunction (x);
+    value_t sumSquares = 0;
+    for (size_t i = 0; i < value_.size(); i++) {
+      value_t y = value_[i];
+      sumSquares += y*y;
+    }
+    result[0] = sumSquares;
+  }
+
+  void SumOfC1Squares::
+  impl_gradient(gradient_t& gradient, const argument_t& x,
+		size_type row) const throw ()
+  {
+    assert (row = 0);
+    computeFunction (x);
+    gradient.clear ();
+    for (size_t i = 0; i < value_.size(); i++) {
+      value_t y = value_[i];
+      baseFunction_->gradient(gradient_, x, i);
+      gradient += 2*y*gradient_;
+    }
+  }
+
+  void SumOfC1Squares::computeFunction (const argument_t x) const
+  {
+    if (x != x_) {
+      x_ = x;
+      (*baseFunction_) (value_, x_);
+    }
+  }
+} // namespace roboptim
