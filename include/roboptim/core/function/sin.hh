52c0bf418ccd2959e2a3962dd5a8cd73a8f1b7d7
Thomas Moulard <thomas.moulard@gmail.com>
2013-08-02 14:32:06 +0900

    Add sinus and cosinus funcitons.

---
+++ include/roboptim/core/function/sin.hh
@@ -0,0 +1,129 @@
+// Copyright (C) 2013 by Thomas Moulard, AIST, CNRS, INRIA.
+//
+// This file is part of the roboptim.
+//
+// roboptim is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// roboptim is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with roboptim.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef ROBOPTIM_CORE_FUNCTION_SIN_HH
+# define ROBOPTIM_CORE_FUNCTION_SIN_HH
+# include <roboptim/core/fwd.hh>
+# include <roboptim/core/twice-differentiable-function.hh>
+# include <roboptim/core/portability.hh>
+
+namespace roboptim
+{
+  /// \addtogroup roboptim_function
+  /// @{
+
+  /// \brief Sin function.
+  template <typename T>
+  class Sin : public GenericTwiceDifferentiableFunction<T>
+  {
+  public:
+    ROBOPTIM_TWICE_DIFFERENTIABLE_FUNCTION_FWD_TYPEDEFS_
+    (GenericTwiceDifferentiableFunction<T>);
+
+    /// \brief Build an constant function.
+    ///
+    /// \param offset constant function offset
+    Sin () throw ()
+      : GenericTwiceDifferentiableFunction<T>
+	(1, 1, "sin")
+    {}
+
+    ~Sin () throw ()
+    {}
+
+    /// \brief Display the function on the specified output stream.
+    ///
+    /// \param o output stream used for display
+    /// \return output stream
+    virtual std::ostream& print (std::ostream& o) const throw ()
+    {
+      return o << "Sin function";
+    }
+
+  protected:
+    void impl_compute (result_t& result, const argument_t& x) const throw ()
+    {
+      result[0] = std::sin (x[0]);
+    }
+
+    void impl_gradient (gradient_t& gradient, const argument_t& x, size_type)
+    const throw ();
+
+    void impl_jacobian (jacobian_t& jacobian, const argument_t& x) const throw ();
+
+    void impl_hessian
+    (hessian_t& hessian, const argument_t& x, size_type) const throw ();
+  };
+
+  template <>
+  inline void
+  Sin<EigenMatrixSparse>::impl_gradient (gradient_t& gradient, const argument_t& x, size_type)
+    const throw ()
+  {
+    gradient.insert (0) = std::cos (x[0]);
+  }
+
+  template <typename T>
+  void
+  Sin<T>::impl_gradient (gradient_t& gradient, const argument_t& x, size_type)
+    const throw ()
+  {
+    gradient[0] = std::cos (x[0]);
+  }
+
+  template <>
+  inline void
+  Sin<EigenMatrixSparse>::impl_jacobian
+  (jacobian_t& jacobian, const argument_t& x) const throw ()
+  {
+    jacobian.insert (0, 0) = std::cos (x[0]);
+  }
+  template <typename T>
+  void
+  Sin<T>::impl_jacobian
+  (jacobian_t& jacobian, const argument_t& x) const throw ()
+  {
+    jacobian (0, 0) = std::cos (x[0]);
+  }
+
+  template <typename T>
+  void
+  Sin<T>::impl_hessian (hessian_t& hessian,
+			const argument_t& x,
+			size_type) const throw ()
+  {
+    hessian (0, 0) = -std::sin (x[0]);
+  }
+
+  template <>
+  inline void
+  Sin<EigenMatrixSparse>::impl_hessian (hessian_t& hessian,
+					const argument_t& x,
+					size_type) const throw ()
+  {
+    hessian.insert (0, 0) = -std::sin (x[0]);
+  }
+
+
+  /// Example shows constant function use.
+  /// \example sin.cc
+
+  /// @}
+
+} // end of namespace roboptim
+
+#endif //! ROBOPTIM_CORE_FUNCTION_SIN_HH
