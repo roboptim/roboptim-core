61e2425eff0981955d0211f3589c32a948b33bd5
Stanislas Brossette <stanislas.brossette@gmail.com>
2013-11-27 19:56:15 +0900

    Implemented some tests for polynomial functions

--- tests/function-polynomial.cc
+++ tests/function-polynomial.cc
@@ -36,19 +36,73 @@ BOOST_FIXTURE_TEST_SUITE (core, TestSuiteConfiguration)
 
 BOOST_AUTO_TEST_CASE_TEMPLATE (polynomial_test, T, functionTypes_t)
 {
-  //test for constant polynomial
+  //test for degree 0 polynomial
   typename Polynomial<T>::vector_t coefficients (1);
-  coefficients[0] = 0;
+  coefficients[0] = 45;
   boost::shared_ptr<Polynomial<T> > fct =
     boost::make_shared<Polynomial<T> > (coefficients);
   fct->print(std::cout);
 
   typename Polynomial<T>::argument_t x (1);
+  x[0] = 0;
+  typename Polynomial<T>::result_t y (1);
+  y[0] = 45;
+  BOOST_CHECK_EQUAL( (*fct) (x), y );
+  
   x[0] = 1;
-  std::cout
-    << (*fct) (x) << "\n"
-    << fct->gradient (x, 0) << "\n"
-    << fct->jacobian (x) << std::endl;
+  y[0] = 45;
+  BOOST_CHECK_EQUAL( (*fct) (x), y );
+
+  //test for degree 1 polynomial 
+  coefficients.resize(2);
+  coefficients[0] = 3;
+  coefficients[1] = 5;
+  fct = boost::make_shared<Polynomial<T> > (coefficients);
+  fct->print(std::cout);
+  
+  x[0] = 0;
+  y[0] = 3;
+  BOOST_CHECK_EQUAL( (*fct) (x), y );
+  x[0] = 1;
+  y[0] = 8;
+  BOOST_CHECK_EQUAL( (*fct) (x), y );
+
+  //test for degree 2 polynomial 
+  coefficients.resize(3);
+  coefficients[0] = 3;
+  coefficients[1] = 5;
+  coefficients[2] = 2;
+  fct = boost::make_shared<Polynomial<T> > (coefficients);
+  fct->print(std::cout);
+  
+  x[0] = 0;
+  y[0] = 3;
+  BOOST_CHECK_EQUAL( (*fct) (x), y );
+  x[0] = 1;
+  y[0] = 10;
+  BOOST_CHECK_EQUAL( (*fct) (x), y );
+  x[0] = 3;
+  y[0] = 36;
+  BOOST_CHECK_EQUAL( (*fct) (x), y );
+
+  //test for degree 3 polynomial 
+  coefficients.resize(4);
+  coefficients[0] = 3;
+  coefficients[1] = 5;
+  coefficients[2] = 2;
+  coefficients[3] = 8;
+  fct = boost::make_shared<Polynomial<T> > (coefficients);
+  fct->print(std::cout);
+  
+  x[0] = 0;
+  y[0] = 3;
+  BOOST_CHECK_EQUAL( (*fct) (x), y );
+  x[0] = 1;
+  y[0] = 18;
+  BOOST_CHECK_EQUAL( (*fct) (x), y );
+  x[0] = 3;
+  y[0] = 252;
+  BOOST_CHECK_EQUAL( (*fct) (x), y );
 }
 
 BOOST_AUTO_TEST_SUITE_END ()
