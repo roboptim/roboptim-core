ab017d8f13829b55e0cac6fd669c264835ca1812
Benjamin Chrétien <chretien@lirmm.fr>
2014-05-04 19:23:41 +0200

    selection-by-id: add throw check in test.

--- tests/filter-selection-by-id.cc
+++ tests/filter-selection-by-id.cc
@@ -58,6 +58,13 @@ BOOST_AUTO_TEST_CASE_TEMPLATE (chain_test, T, functionTypes_t)
     << (*fct) (x) << "\n"
     << fct->gradient (x, 0) << "\n"
     << fct->jacobian (x) << std::endl;
+
+  typename GenericIdentityFunction<T>::result_t offset_throw (6);
+  offset_throw.setZero ();
+  boost::shared_ptr<GenericIdentityFunction<T> > identity_throw =
+    boost::make_shared<GenericIdentityFunction<T> > (offset_throw);
+  BOOST_CHECK_THROW (fct = selectionById (identity_throw, selector),
+                     std::runtime_error);
 }
 
 BOOST_AUTO_TEST_SUITE_END ()
