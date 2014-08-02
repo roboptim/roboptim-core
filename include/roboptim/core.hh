c7237edab8de2e0e0694b26a258b5a52a373412e
Benjamin Chrétien <chretien@lirmm.fr>
2014-08-03 01:26:08 +0200

    core.hh: update headers.

--- include/roboptim/core.hh
+++ include/roboptim/core.hh
@@ -52,14 +52,44 @@
 # include <roboptim/core/util.hh>
 
 
+// Callbacks.
+# include <roboptim/core/callback/multiplexer.hh>
+
+
 // Filters.
+# include <roboptim/core/filter/bind.hh>
 # include <roboptim/core/filter/cached-function.hh>
+# include <roboptim/core/filter/chain.hh>
+# include <roboptim/core/filter/concatenate.hh>
+# include <roboptim/core/filter/derivative.hh>
+# include <roboptim/core/filter/map.hh>
+# include <roboptim/core/filter/minus.hh>
+# include <roboptim/core/filter/plus.hh>
+# include <roboptim/core/filter/product.hh>
+# include <roboptim/core/filter/scalar.hh>
+# include <roboptim/core/filter/selection.hh>
+# include <roboptim/core/filter/selection-by-id.hh>
+# include <roboptim/core/filter/split.hh>
+
+
+// Functions.
+# include <roboptim/core/function/constant.hh>
+# include <roboptim/core/function/cos.hh>
+# include <roboptim/core/function/identity.hh>
+# include <roboptim/core/function/polynomial.hh>
+# include <roboptim/core/function/sin.hh>
 
 
 // Visualization.
 # include <roboptim/core/visualization/gnuplot.hh>
 # include <roboptim/core/visualization/gnuplot-commands.hh>
+# include <roboptim/core/visualization/gnuplot-differentiable-function.hh>
 # include <roboptim/core/visualization/gnuplot-function.hh>
+# include <roboptim/core/visualization/gnuplot-matrix.hh>
+# include <roboptim/core/visualization/matplotlib.hh>
+# include <roboptim/core/visualization/matplotlib-commands.hh>
+# include <roboptim/core/visualization/matplotlib-function.hh>
+# include <roboptim/core/visualization/util.hh>
 
 
 #endif //! ROBOPTIM_CORE_HH
