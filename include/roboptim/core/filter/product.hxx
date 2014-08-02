a69da4869365d044a7995abecbfa1515bda81a1c
Benjamin Chrétien <chretien@lirmm.fr>
2014-08-02 17:00:15 +0200

    filter/product.hxx: fix gradient/Jacobian computation.

--- include/roboptim/core/filter/product.hxx
+++ include/roboptim/core/filter/product.hxx
@@ -18,9 +18,164 @@
 #ifndef ROBOPTIM_CORE_FILTER_PRODUCT_HXX
 # define ROBOPTIM_CORE_FILTER_PRODUCT_HXX
 # include <boost/format.hpp>
+# include <boost/utility/enable_if.hpp>
+# include <boost/type_traits/is_same.hpp>
+# include <boost/mpl/and.hpp>
 
 namespace roboptim
 {
+  namespace detail
+  {
+    /// \brief Utility structure used for product differentiation.
+    struct ProductDifferentiation
+    {
+      /// \brief Some useful types for product differentiation.
+      /// \tparam U left function.
+      /// \tparam V right function.
+      template <typename U, typename V>
+      struct Types
+      {
+        typedef typename boost::mpl::and_<
+          boost::is_same<typename U::traits_t, EigenMatrixDense>,
+          boost::is_same<typename V::traits_t, EigenMatrixDense> >
+            fullDense_t;
+
+        typedef typename Product<U,V>::size_type  size_type;
+        typedef typename Product<U,V>::value_type value_type;
+
+        typedef typename U::vector_t vectorU_t;
+        typedef typename V::vector_t vectorV_t;
+
+        typedef typename U::gradient_t gradientU_t;
+        typedef typename V::gradient_t gradientV_t;
+        typedef typename Product<U,V>::gradient_t gradient_t;
+
+        typedef typename U::jacobian_t jacobianU_t;
+        typedef typename V::jacobian_t jacobianV_t;
+        typedef typename Product<U,V>::jacobian_t jacobian_t;
+      };
+
+
+      /// \brief Full dense version of gradient computation.
+      template <typename U, typename V>
+      static void gradient
+      (typename Types<U,V>::gradient_t& grad_uv,
+       const typename Types<U,V>::vectorU_t& u,
+       const typename Types<U,V>::vectorV_t& v,
+       const typename Types<U,V>::gradientU_t& grad_u,
+       const typename Types<U,V>::gradientV_t& grad_v,
+       typename boost::enable_if<typename Types<U,V>::fullDense_t>::type* = 0)
+      {
+        grad_uv.noalias () = u.cwiseProduct (grad_v);
+        grad_uv.noalias () += v.cwiseProduct (grad_u);
+      }
+
+      /// \brief Dense/sparse version of gradient computation.
+      /// dense.cwiseProduct (sparse) or sparse.cwiseProduct (dense) are
+      /// apparently not available in Eigen.
+      template <typename U, typename V>
+      static void gradient
+      (typename Types<U,V>::gradient_t& grad_uv,
+       const typename Types<U,V>::vectorU_t& u,
+       const typename Types<U,V>::vectorV_t& v,
+       const typename Types<U,V>::gradientU_t& grad_u,
+       const typename Types<U,V>::gradientV_t& grad_v,
+       typename boost::disable_if<typename Types<U,V>::fullDense_t>::type* = 0)
+      {
+        // Here, u and v are dense vectors, while grad_u and grad_v are sparse
+        // vectors. Since cwiseProduct is not available for dense-sparse
+        // computation, we compute it manually. All we need to do is loop over
+        // the nonzeros of grad_u or grad_v and multiply by v or u.
+        grad_uv.setZero ();
+
+        // grad_uv = u * grad_v;
+        for (typename Types<U,V>::gradientV_t::InnerIterator it (grad_v);
+             it; ++it)
+	  {
+	    typename Types<U,V>::gradientV_t::Index id = it.index ();
+	    grad_uv.coeffRef (id) = u (id) * it.value ();
+	  }
+
+        // grad_uv += v * grad_u;
+        for (typename Types<U,V>::gradientU_t::InnerIterator it (grad_u);
+             it; ++it)
+	  {
+	    typename Types<U,V>::gradientU_t::Index id = it.index ();
+	    grad_uv.coeffRef (id) += v (id) * it.value ();
+	  }
+      }
+
+      /// \brief Full dense version of Jacobian computation.
+      template <typename U, typename V>
+      static void jacobian
+      (typename Types<U,V>::jacobian_t& jac_uv,
+       const typename Types<U,V>::vectorU_t& u,
+       const typename Types<U,V>::vectorV_t& v,
+       const typename Types<U,V>::jacobianU_t& jac_u,
+       const typename Types<U,V>::jacobianV_t& jac_v,
+       typename boost::enable_if<typename Types<U,V>::fullDense_t>::type* = 0)
+      {
+        // For each column of the Jacobian
+        // FIXME: do it Eigen-style
+        for (typename Types<U,V>::jacobian_t::Index i = 0;
+             i < jac_uv.cols (); ++i)
+	  {
+	    jac_uv.col (i).noalias () = u.cwiseProduct (jac_v.col (i));
+	    jac_uv.col (i).noalias () += v.cwiseProduct (jac_u.col (i));
+	  }
+      }
+
+      /// \brief Dense/sparse version of Jacobian computation.
+      /// dense.cwiseProduct (sparse) or sparse.cwiseProduct (dense) are
+      /// apparently not available in Eigen.
+      template <typename U, typename V>
+      static void jacobian
+      (typename Types<U,V>::jacobian_t& jac_uv,
+       const typename Types<U,V>::vectorU_t& u,
+       const typename Types<U,V>::vectorV_t& v,
+       const typename Types<U,V>::jacobianU_t& jac_u,
+       const typename Types<U,V>::jacobianV_t& jac_v,
+       typename boost::disable_if<typename Types<U,V>::fullDense_t>::type* = 0)
+      {
+        jac_uv.setZero ();
+
+        typedef Eigen::Triplet<typename Types<U,V>::value_type> triplet_t;
+        std::vector<triplet_t> tripletList;
+        tripletList.reserve
+          (static_cast<std::size_t>
+           (std::max<typename Types<U,V>::size_type>
+            (jac_u.nonZeros (), jac_v.nonZeros ())));
+
+        // tripletList will contain duplicated elements that will be summed up
+        // by setFromTriplets()
+
+        // For each column of the Jacobian
+        // FIXME: do it Eigen-style
+        for (typename Types<U,V>::jacobian_t::Index i = 0;
+             i < jac_uv.cols (); ++i)
+	  {
+	    // grad_uv = u * grad_v;
+	    for (typename Types<U,V>::jacobianV_t::InnerIterator
+		   it (jac_v, i); it; ++it)
+	      {
+		typename Types<U,V>::gradientV_t::Index id = it.index ();
+		tripletList.push_back (triplet_t (id, i, u (id) * it.value ()));
+	      }
+
+	    // grad_uv += v * grad_u;
+	    for (typename Types<U,V>::jacobianU_t::InnerIterator
+		   it (jac_u, i); it; ++it)
+	      {
+		typename Types<U,V>::gradientU_t::Index id = it.index ();
+		tripletList.push_back (triplet_t (id, i, v (id) * it.value ()));
+	      }
+	  }
+
+        jac_uv.setFromTriplets (tripletList.begin (), tripletList.end ());
+      }
+    };
+  } // end of namespace detail.
+
   template <typename U, typename V>
   Product<U, V>::Product
   (boost::shared_ptr<U> left, boost::shared_ptr<V> right)
@@ -70,24 +225,44 @@ namespace roboptim
   template <typename U, typename V>
   void
   Product<U, V>::impl_gradient (gradient_t& gradient,
-				const argument_t& argument,
+				const argument_t& x,
 				size_type functionId)
     const
   {
-    left_->gradient (gradientLeft_, argument, functionId);
-    right_->gradient (gradientRight_, argument, functionId);
-    gradient = gradientLeft_.cwiseProduct (gradientRight_);
+    // Compute grad_U and grad_V
+    left_->gradient (gradientLeft_, x, functionId);
+    right_->gradient (gradientRight_, x, functionId);
+
+    // Compute U and V
+    // FIXME: this is highly inefficient (computed once for each gradient)
+    (*left_) (resultLeft_, x);
+    (*right_) (resultRight_, x);
+
+    // Compute gradient = ∂U V + ∂V U
+    detail::ProductDifferentiation::gradient<U,V>
+      (gradient, resultLeft_, resultRight_,
+       gradientLeft_, gradientRight_);
   }
+
 
   template <typename U, typename V>
   void
   Product<U, V>::impl_jacobian (jacobian_t& jacobian,
-				const argument_t& argument)
+				const argument_t& x)
     const
   {
-    left_->jacobian (jacobianLeft_, argument);
-    right_->jacobian (jacobianRight_, argument);
-    jacobian = jacobianLeft_ * jacobianRight_;
+    // Compute U and V
+    (*left_) (resultLeft_, x);
+    (*right_) (resultRight_, x);
+
+    // Compute Jac(U) and Jac(V)
+    left_->jacobian (jacobianLeft_, x);
+    right_->jacobian (jacobianRight_, x);
+
+    // Compute the Jacobian
+    detail::ProductDifferentiation::jacobian<U,V>
+      (jacobian, resultLeft_, resultRight_,
+       jacobianLeft_, jacobianRight_);
   }
 } // end of namespace roboptim.
 
--- tests/filter-product.cc
+++ tests/filter-product.cc
@@ -28,6 +28,7 @@
 
 #include <roboptim/core/function/constant.hh>
 #include <roboptim/core/function/identity.hh>
+#include <roboptim/core/finite-difference-gradient.hh>
 
 using namespace roboptim;
 
@@ -39,33 +40,75 @@ BOOST_FIXTURE_TEST_SUITE (core, TestSuiteConfiguration)
 
 BOOST_AUTO_TEST_CASE_TEMPLATE (product_test, T, functionTypes_t)
 {
-  typename GenericIdentityFunction<T>::result_t offset (5);
-  offset.setZero ();
+  boost::shared_ptr<boost::test_tools::output_test_stream>
+    output = retrievePattern ("filter-product");
 
+  typename GenericFunction<T>::value_type eps = 1e-6;
+
+  typename GenericIdentityFunction<T>::result_t offset (5);
+
+  offset.setZero ();
   boost::shared_ptr<GenericIdentityFunction<T> > identity =
     boost::make_shared<GenericIdentityFunction<T> > (offset);
+  offset.fill (2.);
   boost::shared_ptr<GenericConstantFunction<T> > constant =
     boost::make_shared<GenericConstantFunction<T> > (offset);
 
   boost::shared_ptr<GenericLinearFunction<T> >
     fct = identity * constant;
+  GenericFiniteDifferenceGradient<T> fd_fct (*fct);
 
   typename GenericIdentityFunction<T>::argument_t x (5);
-  x.setZero ();
+  for (Function::size_type i = 0; i < x.size (); ++i)
+    x[i] = (double)i;
+
   std::cout
-    << (*fct) (x) << "\n"
-    << fct->gradient (x, 0) << "\n"
-    << fct->gradient (x, 1) << "\n"
-    << fct->gradient (x, 2) << "\n"
-    << fct->gradient (x, 3) << "\n"
-    << fct->gradient (x, 4) << "\n"
-    << fct->jacobian (x) << std::endl;
+    << "x = " << x << "\n"
+    << "U(x) = x = " << (*identity) (x) << "\n"
+    << "V(x) = 2 = " << (*constant) (x) << "\n"
+    << "U(x) * V(x) = 2x = " << (*fct) (x) << "\n"
+    << Function::vector_t (fct->gradient (x, 0)) << "\n"
+    << Function::vector_t (fct->gradient (x, 1)) << "\n"
+    << Function::vector_t (fct->gradient (x, 2)) << "\n"
+    << Function::vector_t (fct->gradient (x, 3)) << "\n"
+    << Function::vector_t (fct->gradient (x, 4)) << "\n"
+    << Function::matrix_t (fct->jacobian (x)) << std::endl;
+
+  BOOST_CHECK (allclose (Function::matrix_t (fct->jacobian (x)),
+                         Function::matrix_t (fd_fct.jacobian (x)),
+                         eps, eps));
+
+  std::cout << std::endl;
 
   typename GenericIdentityFunction<T>::result_t offset_throw (6);
   offset_throw.setZero ();
   boost::shared_ptr<GenericConstantFunction<T> > constant_throw =
     boost::make_shared<GenericConstantFunction<T> > (offset_throw);
   BOOST_CHECK_THROW (fct = identity * constant_throw, std::runtime_error);
+
+  // Test gradient with finite differences
+  boost::shared_ptr<GenericDifferentiableFunction<T> >
+    fct2 = fct * fct;
+  GenericFiniteDifferenceGradient<T> fd_fct2 (*fct2);
+
+  std::cout
+    << "x = " << x << "\n"
+    << "U(x) = 2x = " << (*fct) (x) << "\n"
+    << "V(x) = 2x = " << (*fct) (x) << "\n"
+    << "U(x) * V(x) = 4x² = " << (*fct2) (x) << "\n"
+    << Function::vector_t (fct2->gradient (x, 0)) << "\n"
+    << Function::vector_t (fct2->gradient (x, 1)) << "\n"
+    << Function::vector_t (fct2->gradient (x, 2)) << "\n"
+    << Function::vector_t (fct2->gradient (x, 3)) << "\n"
+    << Function::vector_t (fct2->gradient (x, 4)) << "\n"
+    << Function::matrix_t (fct2->jacobian (x)) << std::endl;
+
+  BOOST_CHECK (allclose (Function::matrix_t (fct2->jacobian (x)),
+                         Function::matrix_t (fd_fct2.jacobian (x)),
+                         eps, eps));
+
+  std::cout << output->str () << std::endl;
+  BOOST_CHECK (output->match_pattern ());
 }
 
 BOOST_AUTO_TEST_SUITE_END ()
