0ee7e5cba0f9c71df155d209d38c58dc0b00d560
JRL/IDH Continuous Integration Tool <jrl-idh+ci@gmail.com>
2016-02-15 09:46:02 +0000

    Update HEAD Doxygen documentation. Source commit id: 3f696b4ebccd479652d147331cb0ac7e7cda4654

--- doxygen/HEAD/search/classes_11.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['wrapper',['Wrapper',['../a00158.html',1,'roboptim::callback']]]
-];
--- doxygen/HEAD/search/variables_9.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['jacobian_5f',['jacobian_',['../a00198.html#ac8e4d7d6f9f9af11c249371537ef67fb',1,'function-pool.hxx']]],
-  ['jacobiancache_5f',['jacobianCache_',['../a00050.html#a16acd3875c6cfed5c70faa01e85d980d',1,'roboptim::CachedFunction']]]
-];
