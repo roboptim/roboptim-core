a3174f46f4dcd8b3265ce958405ab06a7aee31fd
Benjamin Chrétien <chretien.b@gmail.com>
2016-05-26 18:42:18 +0200

    Fix compilation on Linux

--- include/roboptim/core/numeric-linear-function.hh
+++ include/roboptim/core/numeric-linear-function.hh
@@ -35,7 +35,7 @@ namespace roboptim
   /// \f[f(x) = A x + b\f]
   /// where \f$A\f$ and \f$b\f$ are set when the class is instantiated.
   template <typename T>
-  class GenericNumericLinearFunction
+  class ROBOPTIM_GCC_ETI_WORKAROUND GenericNumericLinearFunction
   : public GenericLinearFunction<T>
   {
   public:
